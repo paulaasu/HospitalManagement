90396d8c7b109eca2a1d4d7b5071209891ce29a1
maitanegarcia <90905830+maitanegarcia@users.noreply.github.com>
2021-11-15 13:22:44 +0100

    creacion de metodos en la base de datos

--- HospitalManagement/src/clases/BaseDeDatos.java
+++ HospitalManagement/src/clases/BaseDeDatos.java
@@ -1,12 +1,13 @@
 package clases;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.sun.jdi.connect.spi.Connection;
+
 /***
  * 
  * @author BSILV
@@ -14,11 +15,12 @@ import com.sun.jdi.connect.spi.Connection;
  *
  */
 public class BaseDeDatos {
-	public static Connection initBD(String hospitalBD) {
+	public static Connection initBD(String hospitalManagementBD) {
 		Connection con = null;
 		try {
 			Class.forName("org.sqlite.JDBC");
-			con = (Connection) DriverManager.getConnection("jdbc:sqlite:"+hospitalBD);
+			con = (Connection) DriverManager.getConnection("jdbc:sqlite:"+hospitalManagementBD);
+			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -36,23 +38,53 @@ public class BaseDeDatos {
  *
  */
 	public static void closeBD(Connection con) {
-	if(con!=null)	{
+	if(con!=null)
 		try {
 			con.close();
-		} catch (IOException e) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+		
 	
 	}
+	public static void crearTablas(Connection con) {
+		String sent1= "CREATE TABLE IF NOT EXISTS Paciente( dni String,  nombre String,  apellidos String, telefono Integer,  direccion String , "
+				+ "fecha_nacimiento Date, historialClinico HistorialClinico)";
+		String sent2 = "CREATE TABLA IF NOT EXISTS Medico( dni String,  nombre String,  apellidos String,"
+				+ " telefono Integer,  direccion String , fecha_nacimiento Date)" ;
+		String sent3 ="CREATE TABLA IF NOT EXISTS Usuario(dni String,  nombre String,  apellidos String, "
+				+ "telefono Integer,  direccion String , fecha_nacimiento Date, salario Intenger)";
+		String sent4="CREATE TABLA IF NOT EXISTS Informatico(dni String,  nombre String,  apellidos String, telefono Integer,"
+				+ "  direccion String , fecha_nacimiento Date, salario Intenger,cargo String)";
+		String sent5 ="CREATE TABLA IF NO EXISTS Recepcionista(dni String,  nombre String,  apellidos String, telefono Integer, "
+				+ "direccion String , fecha_nacimiento Date, salario Intenger,funciones String)";
+		Statement st = null;
+		
+		try {
+			st = con.createStatement();
+			st.executeUpdate(sent1);
+			st.executeUpdate(sent2);
+			st.executeUpdate(sent3);
+			st.executeUpdate(sent4);
+			st.executeUpdate(sent5);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+	
+	}
+	
 	
 	public static void anadirPacientes(Connection con, String dni, String nombre, String apellidos, 
 Integer telefono, String direccion ,Date fecha_nacimiento,HistorialClinico historialClinico ) {
 		String sentSQL = "INSERT INTO alumno VALUES('"+dni+"','"+nombre+"','"+apellidos+
 				"','+telefono+','"+direccion+"','"+fecha_nacimiento+"','"+historialClinico+")";
 		try {
-			Statement stmt = ((java.sql.Connection) con).createStatement();
+			Statement stmt =null;
+			stmt= con.createStatement();
 			stmt.executeUpdate(sentSQL);
 			stmt.close();
 		} catch (SQLException e) {
@@ -65,7 +97,8 @@ Integer telefono, String direccion ,Date fecha_nacimiento,HistorialClinico histo
 			Integer telefono, String direccion ,Date fecha_nacimiento,HistorialClinico historialClinico) {
 		String sentSQL = "DELETE FROM alumno WHERE dni ='"+dni+"'";
 		try {
-			Statement stmt = ((java.sql.Connection) con).createStatement();
+			Statement stmt = null;
+			stmt= con.createStatement();
 			stmt.executeUpdate(sentSQL);
 			stmt.close();
 		} catch (SQLException e) {
