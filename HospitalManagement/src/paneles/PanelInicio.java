9499831693c825487292dacaae7e7c3987fcf36b
cris.alonso <cris.alonso@Cristina>
2022-01-02 12:07:53 +0100

    Panel Inicio Arreglado

--- HospitalManagement/src/paneles/PanelInicio.java
+++ HospitalManagement/src/paneles/PanelInicio.java
@@ -1,24 +1,30 @@
 package paneles;
 
 import java.awt.BorderLayout;
-import java.awt.Font;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class PanelInicio extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
+	
+	//Como ponemos la foto, el título no es necesario.
 	public PanelInicio() {
+//		setLayout(new BorderLayout());
+//		JLabel lblTitulo=new JLabel("Bienvenido/a a Hospital Management", SwingConstants.CENTER);
+//		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 18));
+//		//panel1.add(lblTitulo,FlowLayout.CENTER);
+//		add(lblTitulo,BorderLayout.NORTH);
+		
 		setLayout(new BorderLayout());
-		JLabel lblTitulo=new JLabel("Bienvenido/a a Hospital Management", SwingConstants.CENTER);
-		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 18));
-		//panel1.add(lblTitulo,FlowLayout.CENTER);
-		add(lblTitulo,BorderLayout.NORTH);
+		ImageIcon img=new ImageIcon("src/img/DeustoHospital.png");
+		JLabel lbl_imagen=new JLabel(img);
+	
+		add(lbl_imagen,BorderLayout.CENTER);
 
 	}
 
